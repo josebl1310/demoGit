5610eba503f1e17c06c18efb4c760e18557d6c67
josebl1310 <josebl1310@gmail.com>
2021-03-20 16:02:21 +0100

    Adios mundo cambiado por dos

--- HolaMundo/src/main/java/holamundo/HolaMundo.java
+++ HolaMundo/src/main/java/holamundo/HolaMundo.java
@@ -11,7 +11,7 @@ package holamundo;
  */
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Adios Mundo");
+        System.out.println("Adios Mundo2");
     }
     
 }
